f95705e702123f6e4b351afed1f8ebf8a21c7919
Sherif Abdalla <sherif@abdalla.uk>
2021-01-06 10:39:07 +0000

    Testing to destruction - A5.2

--- calculator_test.go
+++ calculator_test.go
@@ -23,6 +23,26 @@ func TestAdd1and1(t *testing.T) {
 	}
 }
 
+// Introducing test Case
+func TestAddStruct(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
 func TestSubtract(t *testing.T) {
 	t.Parallel()
 	var want float64 = 2
