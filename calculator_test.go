afa0613396fbdb97a46914bd3c03649348ac3b8c
Sherif Abdalla <sherif@abdalla.uk>
2021-01-07 10:59:52 +0000

    Testing to destruction - A5.3
    Failed

--- calculator_test.go
+++ calculator_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
+type testCases struct {
+	a, b float64
+	want float64
+	name string
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
 	var want float64 = 4
@@ -23,22 +29,24 @@ func TestAdd1and1(t *testing.T) {
 	}
 }
 
-// Introducing test Case
+// Introducing A slice of test Cases
 func TestAddStruct(t *testing.T) {
 	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
-	testCases := []testCase{
+	testCases := []*testCase{
 		{a: 2, b: 2, want: 4},
-		{a: 1, b: 1, want: 2},
-		{a: 5, b: 0, want: 5},
+		{a: -1, b: -1, want: -2},
+		{a: -5, b: 0, want: 0},
+		{a: 2, b: 2, want: 4, name: "Two postive numbers that sum to a positive"},
+		{a: -1, b: -1, want: -2, name: "Two negative numbers that sum ot a negative"},
+		{a: -5, b: 0, want: 0, name: "Negative and positive number that sum Zero"},
 	}
+
+	// Loopig over test cases
 	for _, tc := range testCases {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
-			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+			// t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+			t.Errorf("want %f, got %f", tc.want, tc.name, got)
 		}
 	}
 }
