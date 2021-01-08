fe98df58f880ddd97fa39ed7246045b1ff1547b3
Sherif Abdalla <sherif@abdalla.uk>
2021-01-08 12:56:23 +0000

    errors and expectations - A6.3
    minimum code necessary to make TestDivide pass >> (FAILED)

--- calculator.go
+++ calculator.go
@@ -1,6 +1,8 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "fmt"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -15,4 +17,12 @@ func Subtract(a, b float64) float64 {
 // Multiply takes two numbers as parameters, and returns a single number representing the result.
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two numbers as parameters, and returns first divide by second representing the result.
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("division by zero is undefined")
+	}
+	return a / b, nil
 }
