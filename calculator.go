ed7ef4cc2b224abbcf6c63ca4751ff0cb960cb99
Sherif Abdalla <sherif@abdalla.uk>
2021-01-08 20:35:29 +0000

    simple string-based error

--- calculator.go
+++ calculator.go
@@ -1,7 +1,7 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
-import "fmt"
+import "errors"
 
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
@@ -22,7 +22,7 @@ func Multiply(a, b float64) float64 {
 // Divide takes two numbers as parameters, and returns first divide by second representing the result.
 func Divide(a, b float64) (float64, error) {
 	if b == 0 {
-		return 0, fmt.Errorf("division by zero is undefined")
+		return 0, errors.New("division by zero is undefined")
 	}
 	return a / b, nil
 }
